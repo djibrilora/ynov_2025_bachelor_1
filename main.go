d7cb3ed82d6fc1babc0d17c6a2be01c3d9f951d0
first commit <djibril@Djibril.localdomain>
2025-09-22 17:33:07 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age uint8
+	var prenom string
+	fmt.Print("Quel est votre âge ?")
+	fmt.Print("\n")
+	fmt.Scan(&age)
+	fmt.Print("Quel est votre prénom ?")
+	fmt.Print("\n")
+	fmt.Scan(&prenom)
+	if age >= 18 {
+		fmt.Print("Vous êtes majeur, voici votre document !")
+		fmt.Print("\n")
+	} else {
+		fmt.Print("Vous êtes mineur, rentrez chez vous !")
+		fmt.Print("\n")
+	}
+}
